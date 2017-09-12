729dcff148570b53474205c3f45352edb99771df
wallytreats <nicklawrenceemail@gmail.com>
2017-09-12 10:18:32 -0600

    all routes validated

--- routes/users.js
+++ routes/users.js
@@ -16,11 +16,42 @@ router.get('/' , (req, res, next) => {
 });
 
 router.post('/' , (req, res, next) => {
-  let firstName = req.body.users.firstName;
-  let lastName = req.body.users.lastName;
-  let username = req.body.users.username;
-  let email = req.body.users.email;
-  let phone = req.body.users.phone;
+  const { firstName, lastName, username ,email, phone } = req.body.users;
+
+    if (!firstName) {
+      const err = new Error('Firstname must not be blank');
+      err.status = 400;
+
+      return next(err);
+    }
+
+    if (!lastName) {
+      const err = new Error('Lastname must not be blank');
+      err.status = 400;
+
+      return next(err);
+    }
+
+    if (!username || 6 >= username.length || !/^([a-zA-Z0-9])$/.test(username) || !/^[a-zA-Z]$/.test(username[0])) {
+      const err = new Error('Username must not be blank');
+      err.status = 400;
+
+      return next(err);
+    }
+
+    if (!email || email.trim() === '' || email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+      const err = new Error('Email must not be blank');
+      err.status = 400;
+
+      return next(err);
+    }
+
+    if (!phone || 10 < phone.length || 10 > phone.length) {
+      const err = new Error('Phone must not be blank');
+      err.status = 400;
+
+      return next(err);
+    }
 
   knex('users')
     .insert({
